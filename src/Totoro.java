509f7c58b2d167a9a16cfe5175ed05ab2a1c1813
cyonkee <cyonkee@gmail.com>
2017-03-14 15:21:30 -0400

    Added Totoro Class.

---
+++ src/Totoro.java
@@ -0,0 +1,29 @@
+/**
+ * Created by cyonkee on 3/14/17.
+ */
+public class Totoro {
+    private String status;
+    private String color;
+    private int score;
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+    public String getStatus(){
+        return status;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+    public int getScore(){
+        return score;
+    }
+}
