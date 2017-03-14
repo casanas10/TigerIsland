11ad4b39a6544e3aee20ea5de8f74ff9002e248e
Alejandro Casanas <casanas10@gmail.com>
2017-03-14 15:15:38 -0400

    Add terrain class

---
+++ src/Terrain.java
@@ -0,0 +1,21 @@
+/**
+ * Created by alecasanas on 3/14/17.
+ */
+public class Terrain {
+    private String type;
+    private boolean isFounded;
+
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getType(){
+        return type;
+    }
+
+    public void setIsFounded(boolean isFounded){
+        this.isFounded = isFounded;
+    }
+    public boolean getIsFounded(){
+        return isFounded;
+    }
+}
