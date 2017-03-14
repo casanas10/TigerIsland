cabd586e0980326eb2b1d6c41f1ac05c93a5d5aa
Alejandro Casanas <casanas10@gmail.com>
2017-03-14 14:36:17 -0400

    add meeple class

---
+++ src/Meeple.java
@@ -0,0 +1,17 @@
+/**
+ * Created by alecasanas on 3/14/17.
+ */
+public class Meeple {
+    private String status;
+    private String color;
+    private int score;
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+    public String getStatus(){
+        return status;
+    }
+
+
+}
