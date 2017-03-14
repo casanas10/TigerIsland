85131bf5a590e31d3ec4b26d7f57adb8d5df3d67
Alejandro Casanas <casanas10@gmail.com>
2017-03-14 16:12:37 -0400

    add the main class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by alecasanas on 3/14/17.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.print("hi");
+    }
+}
--- src/Tile.java
+++ src/Tile.java
@@ -24,6 +24,4 @@ public class Tile {
         return terrains;
     }
 
-
-
 }
