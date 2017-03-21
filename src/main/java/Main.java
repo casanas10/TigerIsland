935f243dc62e2886399ccee68131f192e455a998
Connor <connorericson@ufl.edu>
2017-03-21 12:01:41 -0400

    CoordinateSystem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@
  * Created by alecasanas on 3/14/17.
  */
 public class Main {
-
     public static void main(String[] args){
 //        System.out.println("Game Starts");
 //        Game game = new Game();
