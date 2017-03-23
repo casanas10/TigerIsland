f4b1c214d338df44a64b5a0adfadc01e834bb57f
Alejandro Casanas <casanas10@gmail.com>
2017-03-22 23:51:39 -0400

    hex grid

--- test/HexGridTest.java
+++ test/HexGridTest.java
@@ -13,11 +13,8 @@ public class HexGridTest {
 
         hexGrid.generateHexGrid();
 
-<<<<<<< HEAD
+
         Hex hexo = hexGrid.getHexValue(26);
-=======
-        Hex hexo = hexGrid.getHexValue(200);
->>>>>>> d498c4f8b30d33cf7e6d948891d056b34abe729d
 
         Assert.assertEquals(0,hexo.getX());
         Assert.assertEquals(1,hexo.getY());
