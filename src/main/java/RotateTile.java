de35bee78fe8f18b07ff3c9e040015dd8e76b0c2
Alejandro Casanas <casanas10@gmail.com>
2017-03-23 00:10:15 -0400

    Updated the Junit test cases

---
+++ src/main/java/RotateTile.java
@@ -0,0 +1,42 @@
+/**
+ * Created by Val on 3/20/2017.
+ */
+public class RotateTile {
+
+    int HexID;
+    int[] Tile = new int[3];
+    int Orientation;
+
+    RotateTile(int HexID, int orientation) {
+        this.Orientation = orientation;
+        this.HexID = HexID;
+    }
+
+    public int[] checkPair(){
+
+        int[] Hex1Offset = new int[6];
+
+        Hex1Offset[0] = 200;
+        Hex1Offset[1] = 201;
+        Hex1Offset[2] = 1;
+        Hex1Offset[3] = -199;
+        Hex1Offset[4] = -200;
+        Hex1Offset[5] = -1;
+
+        int[] Hex2Offset = new int[6];
+
+        Hex2Offset[0] = 201;
+        Hex2Offset[1] = 1;
+        Hex2Offset[2] = -199;
+        Hex2Offset[3] = -200;
+        Hex2Offset[4] = -1;
+        Hex2Offset[5] = 200;
+
+        Tile[0] = HexID;
+        Tile[1] = this.HexID + Hex1Offset[Orientation/60];
+        Tile[2] = this.HexID + Hex2Offset[Orientation/60];
+
+        return Tile;
+
+    }
+}
