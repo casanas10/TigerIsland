10a1550d051095db10c614d0fbcb32f3686009c2
cyonkee <cyonkee@gmail.com>
2017-04-02 15:47:21 -0400

    Working on threading. Created runnable class that runs two games at once.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.awt.*;
  * Created by alecasanas on 3/14/17.
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -13,7 +13,14 @@ public class Main {
             }
         });
 
-        Game game = new Game();
-        game.gameRunning();
+        MyRunnable R1 = new MyRunnable("Thread for Game 1");
+        R1.start();
+
+        MyRunnable R2 = new MyRunnable("Thread for Game 2");
+        R2.start();
+
+//    Game game = new Game();
+//        game.gameRunning();
+//    }
     }
 }
